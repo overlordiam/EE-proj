ddc9fa0e4ec10cf6e83e58d84decb3cdc019d38f
Tharun sekar <tharunsd23@gmail.com>
2020-11-06 15:17:18 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA81JhJ0UIQF9BtZAyJGkRdWX9E2Gsy7EE",
+  authDomain: "clone-81d17.firebaseapp.com",
+  databaseURL: "https://clone-81d17.firebaseio.com",
+  projectId: "clone-81d17",
+  storageBucket: "clone-81d17.appspot.com",
+  messagingSenderId: "935287167093",
+  appId: "1:935287167093:web:5def0fbc4f7928178ac03c",
+  measurementId: "G-CQ2KVC1HJD",
+};
